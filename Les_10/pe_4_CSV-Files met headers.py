3f8f9a357de531a7e714edda13c7aa52b6c249d4
bgeer <0932933@hr.nl>
2018-10-08 13:44:57 +0200

    les 10 af

--- Les_10/pe_4_CSV-Files met headers.py
+++ Les_10/pe_4_CSV-Files met headers.py
@@ -13,5 +13,55 @@ def fmaken():
     writer.writerow([271, 'TRS665', 'Ipad hoes', 155, 19.01])
 
 
-def
-fmaken()
+def duurtsteartikel():
+    bestand = 'index.csv'
+    file = open(bestand, 'r')
+    reader = csv.reader(file, delimiter =',')
+    price = [" ", " ", " ", " ", 0];
+    next(reader)
+    for row in reader:
+        if float(row[4]) > float(price[4]):
+            price = row;
+    highestprice = price[4]
+    product = price[2]
+    # print(highestprice, product)
+    return product, highestprice
+
+def loweststock():
+    bestand = 'index.csv'
+    file = open(bestand, 'r')
+    reader = csv.reader(file, delimiter=',')
+    stock = [" ", " ", " ", float('inf'), " "];
+    next(reader)
+    for row in reader:
+        if float(row[3]) < float(stock[3]):
+            stock = row;
+    loweststock = stock[3]
+    productid = stock[0]
+    # print(loweststock, productid)
+    return loweststock, productid
+
+def stock():
+    bestand = 'index.csv'
+    file = open(bestand, 'r')
+    reader = csv.reader(file, delimiter=',')
+    stock = 0
+    next(reader)
+    for row in reader:
+         stock += float(row[3])
+    # print(int(stock))
+    return int(stock)
+
+def printall():
+    pname, pprice = duurtsteartikel()
+    plowstock, pid = loweststock()
+    totalstock = stock()
+    print("Het duurste artikel is ", pname, "en die kost ", pprice, " Euro")
+    print("Er zijn slechts ", plowstock, " exemplaren in voorraad van het product met nummer ", pid)
+    print("In totaal hebben wij ", totalstock, " producten in ons magazijn liggen")
+
+# fmaken()
+# duurtsteartikel()[:1]
+# loweststock()
+# stock()
+printall()
